98abc5da64dd0bd3f8883202bfbcf037f5b888a5
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-22 18:35:27 +0200

    add Login Page & connect to cognito

---
+++ src/containers/Login.js
@@ -0,0 +1,51 @@
+import "./Login.css";
+import React, { useState } from "react";
+import { Auth } from "aws-amplify";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+        <Button block disabled={!validateForm()} type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
