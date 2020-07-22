b7a7b4628b18f805a7d6f0d8aae51e1ce6f641e4
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-22 15:57:22 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "dev-notes-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://w2k82vlkwa.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_TmqYGy44Q",
+        APP_CLIENT_ID: "2tlapgfmdoq1lhb3pk4ic70oko",
+        IDENTITY_POOL_ID: "us-east-2:8186e514-90c8-41bb-9406-4bac929570b5"
+    }
+};
