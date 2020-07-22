05abc47a645293a6e234a3074192f273ba14d662
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-22 16:02:23 +0200

    remove unused Router

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 
