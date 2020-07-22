ac65671c2ee5ab28e69e6b277d2e44ce40b3bdd5
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-22 15:22:16 +0200

    add react router configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, Nav } from "react-bootstrap";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar bg="light" collapseOnSelect>
+        <Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Scratch</Nav.Link>
+          </Nav>
+        </Navbar.Brand>
+
+      </Navbar>
     </div>
   );
 }
