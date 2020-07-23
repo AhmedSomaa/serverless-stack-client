d8ccda2509cfc763b3b33bdbf412135bef6decb7
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-23 17:16:27 +0200

    Call the list API

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
 import { onError } from "../libs/errorLib";
 import { useAppContext } from "../libs/contextLib";
-import {
-  PageHeader,
-  ListGroup,
-  ListGroupItem,
-  Dropdown,
-} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { ListGroup, ListGroupItem, Dropdown } from "react-bootstrap";
 import "./Home.css";
 
 export default function Home() {
@@ -14,8 +11,48 @@ export default function Home() {
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (error) {
+        onError(error);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  function loadNotes() {
+    return API.get("notes", "/notes");
+  }
+
   function renderNoteList(notes) {
-    return null;
+    return [{}].concat(notes).map((note, i) =>
+      i !== 0 ? (
+        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+          <ListGroupItem>
+            <h4>{note.content.trim().split("\n")[0]}</h4>
+            {"Created: " + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        </LinkContainer>
+      ) : (
+        <LinkContainer key="new" to="/notes/new">
+          <ListGroupItem>
+            <h4>
+              <b>{"\uFF0B"}</b> create a new note
+            </h4>
+          </ListGroupItem>
+        </LinkContainer>
+      )
+    );
   }
 
   function renderLander() {
