b791ff81824f173733c805a94af4c1fbd5deafc7
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-23 12:14:33 +0200

    load state from session & clear session on logout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
-import React, { useState } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Auth } from "aws-amplify";
 import Routes from "./Routes";
+import { Navbar, Nav } from "react-bootstrap";
 import { AppContext } from "./libs/contextLib";
 
 function App() {
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
 
-  function handleLogout() {
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (error) {
+      if (error !== "No current user") {
+        alert(error);
+      }
+      setIsAuthenticating(false);
+    }
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
     userHasAuthenticated(false);
   }
 
   return (
-    <div className="App container">
-      <Navbar bg="light" collapseOnSelect>
-        <Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Scratch</Nav.Link>
-          </Nav>
-        </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            {isAuthenticated ? (
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-            ) : (
-              <>
-                <Nav.Link href="/signup">Signup</Nav.Link>
-                <Nav.Link href="login">Login</Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-        <Routes />
-      </AppContext.Provider>
-    </div>
+    !isAuthenticating && (
+      <div className="App container">
+        <Navbar bg="light" collapseOnSelect>
+          <Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Scratch</Nav.Link>
+            </Nav>
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <Nav.Link href="/signup">Signup</Nav.Link>
+                  <Nav.Link href="login">Login</Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
