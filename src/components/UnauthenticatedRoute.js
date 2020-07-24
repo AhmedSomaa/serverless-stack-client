f5af3e71d25bcdb491f81bb5297a3c991136ea62
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-24 14:09:23 +0200

    fix typo

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -22,12 +22,12 @@ export default function UnauthenticatedRoute({ children, ...rest }) {
   const { isAuthenticated } = useAppContext();
   const redirect = querystring("redirect");
   return (
-    <route {...rest}>
+    <Route {...rest}>
       {!isAuthenticated ? (
         children
       ) : (
         <Redirect to={redirect === "" || redirect === null ? "/" : redirect} />
       )}
-    </route>
+    </Route>
   );
 }
